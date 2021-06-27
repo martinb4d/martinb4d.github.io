cc6fe67f32d9387ce790d5199a2d5182ee135fa4
Martinb4d <martinb4d@gmail.com>
2021-06-27 20:06:21 +0700

    prepare deployment

--- server/server.js
+++ server/server.js
@@ -4,10 +4,11 @@ const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 
-app.use(express.static(publicPath));
+app.use(express.static('build'));
 
 app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
+//    res.sendFile(path.join(publicPath, 'index.html'));
+   res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 app.listen(port, () => {
    console.log('Server is up!');
