b054247f42a46f6014aef008a4a8cddbe6277a7d
Martinb4d <martinb4d@gmail.com>
2021-06-27 21:11:37 +0700

    fix error refresh

--- server/server.js
+++ server/server.js
@@ -1,13 +1,12 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static('build'));
 
 app.get('*', (req, res) => {
-//    res.sendFile(path.join(publicPath, 'index.html'));
    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 app.listen(port, () => {
