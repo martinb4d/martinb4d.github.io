ad8b3b1a6389d16495905c9ef0261be8cced6078
Martinb4d <martinb4d@gmail.com>
2021-06-27 22:18:19 +0700

    remove unused content

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-console.log('__dirname',path.join(__dirname, 'build'));
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', function (req, res) {
    res.sendFile(path.join(__dirname, 'build', 'index.html'));
