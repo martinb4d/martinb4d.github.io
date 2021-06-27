eacf616d93f26d900487b873f68faec474b451e7
Martinb4d <martinb4d@gmail.com>
2021-06-27 21:55:10 +0700

    setup deployment

--- server/server.js
+++ server/server.js
@@ -2,10 +2,10 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-
-app.use(express.static(path.join(__dirname, 'build')));
+console.log('__dirname',__dirname);
+app.use(express.static(path.join('.', 'build')));
 app.get('/*', function (req, res) {
-   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+   res.sendFile(path.join('.', 'build', 'index.html'));
 });
 
 app.listen(port, () => {
