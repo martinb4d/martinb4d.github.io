679ce4c9e9aac3f19946b3f75926e7e21f780bc5
Martinb4d <martinb4d@gmail.com>
2021-06-27 21:23:28 +0700

    fix reload

--- server/server.js
+++ server/server.js
@@ -3,12 +3,20 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static('build'));
 
 app.get('*', (req, res) => {
    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
+
 app.listen(port, () => {
    console.log('Server is up!');
 });
